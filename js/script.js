5b0706ae18ff6a6ae8fca8081f4ab6c82436c128
Artyom <art161994@yandex.ru>
2021-11-30 22:21:51 +0300

    add functionality contact forms work with JSON adn php.server with Open server panel

--- js/script.js
+++ js/script.js
@@ -38,7 +38,7 @@ window.addEventListener('DOMContentLoaded', () => {
     });
 
     //Timer
-    const deadline = '2021-11-22';
+    const deadline = '2021-12-30';
 
     function getTimeRemaining(endtime) {
         const t = Date.parse(endtime) - Date.parse(new Date()),
@@ -130,7 +130,7 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     });
 
-    //const modalTimerId = setTimeout(openModal, 3000);
+    const modalTimerId = setTimeout(openModal, 3000);
 
     function showModalByScroll() {
         if (window.pageYOffset + document.documentElement.clientHeight >=
@@ -145,14 +145,15 @@ window.addEventListener('DOMContentLoaded', () => {
     // Use classes for food cards
 
     class MenuCard {
-        constructor(src, alt, title, descr, price, parentSelector) {
+        constructor(src, alt, title, descr, price, parentSelector, ...classes) {
             this.src = src;
             this.alt = alt;
             this.title = title;
             this.descr = descr;
             this.price = price;
+            this.classes = classes;
             this.parent = document.querySelector(parentSelector);
-            this.transfer = 2.40;
+            this.transfer = 2.50;
             this.changeToBYN();
         }
 
@@ -162,8 +163,15 @@ window.addEventListener('DOMContentLoaded', () => {
 
         render() {
             const element = document.createElement('div');
+            if (this.classes.length === 0) {
+                this.element = 'menu__item';
+                element.classList.add(this.element);
+            } else {
+                this.classes.forEach(className => element.classList.add(className));
+            }
+
+
             element.innerHTML = `
-            <div class = "menu__item">
                 <img src = ${this.src} alt = ${this.alt} >
                 <h3 class = "menu__item-subtitle" > ${this.title} </h3> 
                 <div class = "menu__item-descr" > ${this.descr} </div> 
@@ -173,7 +181,6 @@ window.addEventListener('DOMContentLoaded', () => {
                 <div class = "menu__item-total"> <span> ${this.price} </span> руб/день 
                 </div> 
                 </div> 
-            </div>
             `;
             this.parent.append(element);
         }
@@ -187,27 +194,83 @@ window.addEventListener('DOMContentLoaded', () => {
         овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной
         ценой и высоким качеством!`,
         10,
-        '.menu .container'
+        '.menu .container',
+        'menu__item'
     ).render();
 
     new MenuCard(
         "img/tabs/post.jpg",
         "post",
         'Меню "Постное"',
-        `Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, 
+        `Меню “Постное” - это тщательный подбор ингредиентов: 
+        полное отсутствие продуктов животного происхождения, 
         молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и 
         импортных вегетарианских стейков.`,
         14,
-        ".menu .container"
+        ".menu .container",
+        'menu__item'
     ).render();
 
     new MenuCard(
         "img/tabs/elite.jpg",
         "elite",
         'Меню “Премиум”',
-        `В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. 
+        `В меню “Премиум” мы используем не только красивый дизайн упаковки, 
+        но и качественное исполнение блюд. 
         Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!`,
         21,
-        ".menu .container"
+        ".menu .container",
+        'menu__item'
     ).render();
+
+    // Forms
+
+    const forms = document.querySelectorAll('form');
+
+    const message = {
+        loading: 'Loading',
+        success: 'Thank you, we will contact you soon',
+        failure: 'Something wrong...'
+    };
+
+    forms.forEach(item => {
+        postData(item);
+    });
+
+    function postData(form) {
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            const statusMessage = document.createElement('div');
+            statusMessage.classList.add('status');
+            statusMessage.textContent = message.loading;
+            form.append(statusMessage);
+
+            const request = new XMLHttpRequest();
+            request.open('POST', 'server.php');
+
+            request.setRequestHeader('Content-type', 'application/json');
+            const formData = new FormData(form);
+
+            const object = {};
+            formData.forEach(function (value, key) {
+                object[key] = value;
+            });
+            const json = JSON.stringify(object);
+
+            request.send(json);
+            request.addEventListener('load', () => {
+                if (request.status === 200) {
+                    console.log(request.response);
+                    statusMessage.textContent = message.success;
+                    form.reset();
+                    setTimeout(() => {
+                        statusMessage.remove();
+                    }, 3000);
+                } else {
+                    statusMessage.textContent = message.failure;
+                }
+            });
+        });
+    }
 });
