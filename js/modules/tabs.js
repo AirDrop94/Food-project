057d07fd8d5118f74c1c3c0901903914c159e6b7
Artyom <art161994@yandex.ru>
2022-01-23 23:44:49 +0300

    add module structure on project, add webpack

---
+++ js/modules/tabs.js
@@ -0,0 +1,41 @@
+function tabs(tabsSelector, tabsContectSelector, tabsParentSelector, activeClass) {
+
+    const tabs = document.querySelectorAll(tabsSelector),
+        tabsContent = document.querySelectorAll(tabsContectSelector),
+        tabsParent = document.querySelector(tabsParentSelector);
+
+    function hideTabsContent() {
+        tabsContent.forEach(item => {
+            item.classList.add('hide');
+            item.classList.remove('show', 'fade');
+        });
+
+        tabs.forEach(item => {
+            item.classList.remove(activeClass);
+        });
+    }
+
+    function showTabContent(i = 0) {
+        tabsContent[i].classList.add('show', 'fade');
+        tabsContent[i].classList.remove('hide');
+        tabs[i].classList.add(activeClass);
+    }
+
+    hideTabsContent();
+    showTabContent();
+
+    tabsParent.addEventListener('click', (event) => {
+        const target = event.target;
+
+        if (target && target.classList.contains(tabsSelector.slice(1))) {
+            tabs.forEach((item, i) => {
+                if (target == item) {
+                    hideTabsContent();
+                    showTabContent(i);
+                }
+            });
+        }
+    });
+}
+
+export default tabs;
